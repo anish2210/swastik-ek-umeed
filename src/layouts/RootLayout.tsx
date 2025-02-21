de9557635b7ac985f07288fb6cdce4dd708dce7a
anish2210 <anishjaiswal1220@gmail.com>
2025-02-21 23:14:52 +0530

    resolved error caused while build

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,15 @@
-import { Toaster } from "@/components/ui/toaster"
+import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster";
 
-export default function RootLayout({ children }) {
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       {children}
       <Toaster />
     </>
-  )
-} 
+  );
+}
